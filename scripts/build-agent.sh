e5d4d7206ca688e0743d4a1da8c8cbab7525c0de
PINK <henghengrahman@gmail.com>
2026-09-05 14:07:37 +0700

    Delete scripts directory

--- scripts/build-agent.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env sh
-set -eu
-mkdir -p dist
-cd agent
-GOOS=windows GOARCH=amd64 CGO_ENABLED=0 go build -trimpath -ldflags="-s -w -H=windowsgui" -o ../dist/StaffMonitorAgent.exe .
-echo "Built dist/StaffMonitorAgent.exe"
